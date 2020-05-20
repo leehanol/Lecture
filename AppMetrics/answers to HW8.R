651233fed4d686eecdd8c1d28ab0e50f763e1dd6
leehanol <62641467+leehanol@users.noreply.github.com>
2020-05-20 19:08:39 +0900

    Update answers to HW8.R

--- AppMetrics/answers to HW8.R
+++ AppMetrics/answers to HW8.R
@@ -5,8 +5,8 @@ head(data)
 result <- lm(log(salary)~log(roe)+log(sales),data=data)
 summary(result)
 
-lroesq <- log(data$roe)^2
+lroesq <- (log(data$roe))^2
 
 result2 <- lm(log(salary)~log(roe)+lroesq+log(sales),data=data)
-summary(result)
+summary(result2)
 
