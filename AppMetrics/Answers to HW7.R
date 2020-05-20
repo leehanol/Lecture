331a067696042d83c9467d8e1e173b2b841eb668
leehanol <62641467+leehanol@users.noreply.github.com>
2020-05-20 18:31:58 +0900

    Update Answers to HW7.R

--- AppMetrics/Answers to HW7.R
+++ AppMetrics/Answers to HW7.R
@@ -2,7 +2,7 @@ data <-read.csv("table2.csv")
 head(data)
 
 
-result1 <- lm(wage ~ female, data=data)
+result1 <- lm(wage ~ educ+ female, data=data)
 summary(result1)
 
 result2 <- lm(log(wage) ~ educ+female, data=data)
