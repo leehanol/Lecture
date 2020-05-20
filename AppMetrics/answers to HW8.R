c6af00719959bfcd78b9a5885e8eb8b0c6bf6ca5
leehanol <62641467+leehanol@users.noreply.github.com>
2020-05-20 18:27:11 +0900

    Add files via upload

---
+++ AppMetrics/answers to HW8.R
@@ -0,0 +1,12 @@
+ls()
+desc
+head(data)
+
+result <- lm(log(salary)~log(roe)+log(sales),data=data)
+summary(result)
+
+lroesq <- log(data$roe)^2
+
+result2 <- lm(log(salary)~log(roe)+lroesq+log(sales),data=data)
+summary(result)
+
