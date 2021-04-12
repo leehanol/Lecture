007b862e75da8efcbbceaba41201473c19199791
leehanol <62641467+leehanol@users.noreply.github.com>
2021-04-13 07:58:44 +0800

    Delete AppMetrics directory

--- AppMetrics/final/README.md
+++
@@ -1,1 +0,0 @@
-Final exam, Spring 2020
--- AppMetrics/scatter diagram.r
+++
@@ -1,7 +0,0 @@
-data <-read.csv("table2.csv")
-head(data)
-
-scatter <- plot(x=data$educ, y=data$wage)
-scatter2 <- plot(formula=wage~educ, data=data)
-scatter_titled <- plot(formula=wage~educ, data=data, main="Scatter Diagram", xlab="Years of education", ylab="Hourly wage")
-
