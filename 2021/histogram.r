1f77aa0d1fdad30fdc54db781ab758ac002f9c40
leehanol <62641467+leehanol@users.noreply.github.com>
2021-04-13 08:07:33 +0800

    Add files via upload

--- 2021/histogram.r
+++ 2021/histogram.r
@@ -1,11 +1,5 @@
 data <-read.csv("table2.csv")
 head(data)
 
-hist_wage <-hist(data$wage)
-hist_wage_titled <-hist(data$wage,xlab="Hourly Wage", ylab="Frequency", main="Distribution of Hourly Wage")
-hist_wage_bk5 <-hist(data$wage,breaks=6)
-hist_wage_bk3 <-hist(data$wage,breaks=3)
-hist_wage_bp <- hist(data$wage,breaks=c(15,20,25,30,35))
-hist_wage_bp2 <- hist(data$wage,breaks=seq(15,35,by=5))
-
-hist_wage_density <-hist(data$wage,freq=FALSE)
+hist_wage <-hist(data$wage,xlab="Hourly Wage", ylab="Frequency", main="Distribution of Hourly Wage")
+hist_wage_density <-hist(data$wage,freq=FALSE,xlab="Hourly Wage", ylab="Frequency", main="Distribution of Hourly Wage")
