106f08ce174767afa99d6a3c7d7a32d01c6ac6c9
leehanol <62641467+leehanol@users.noreply.github.com>
2021-04-20 08:07:26 +0800

    Add files via upload

---
+++ 2021/bar chart.r
@@ -0,0 +1,8 @@
+data <-read.csv("table2.csv")
+head(data)
+
+barchart(data$female, horizontal = FALSE, ylab = "Frequency")
+
+barchart(data$female/nrow(data$female), horizontal = FALSE, ylab = "Relative Frequency")
+
+barchart(data$female/nrow(data$female)*100, horizontal = FALSE, ylab = "Percentage")
